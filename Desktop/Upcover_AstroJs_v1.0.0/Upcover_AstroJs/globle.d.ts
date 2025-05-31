e2588859d67bad1dc49720ebe7914c1bd507a1db
Steven Wagner <steven@sales-ignition.com>
2025-05-31 19:10:47 -0400

    Initial commit: Astro.js project setup

---
+++ Desktop/Upcover_AstroJs_v1.0.0/Upcover_AstroJs/globle.d.ts
@@ -0,0 +1,16 @@
+declare module "tobii" {
+    export default class Tobii {
+        constructor(options?: any);
+    }
+}
+
+declare module "gumshoejs" {
+    export default class Gumshoe {
+static init: any;
+static init() {
+throw new Error("Method not implemented.");
+}
+      constructor(selector: string, options?: Record<string, any>);
+    }
+  }
+  
---
+++ Desktop/Upcover_AstroJs_v1.0.0/Upcover_AstroJs/src/data/data.ts
@@ -0,0 +1,161 @@
+import client1 from '../assets/images/team/01.jpg'
+import client2 from '../assets/images/team/02.jpg'
+import client3 from '../assets/images/team/03.jpg'
+import client4 from '../assets/images/team/04.jpg'
+import client5 from '../assets/images/team/05.jpg'
+import client6 from '../assets/images/team/06.jpg'
+
+import blog1 from '../assets/images/blog/01.jpg'
+import blog2 from '../assets/images/blog/02.jpg'
+import blog3 from '../assets/images/blog/03.jpg'
+
+import partner1 from '../assets/images/team/amazon.svg'
+import partner2 from '../assets/images/team/google.svg'
+import partner3 from '../assets/images/team/lenovo.svg'
+import partner4 from '../assets/images/team/paypal.svg'
+import partner5 from '../assets/images/team/shopify.svg'
+import partner6 from '../assets/images/team/spotify.svg'
+
+export const servicesData = [
+    {
+        icon:'trending-up',
+        title:'Grow Your Business',
+        desc:`If the distribution of letters and 'words' is random, the reader will not be distracted from making.`
+    },
+    {
+        icon:'dollar-sign',
+        title:'Drive More Sales',
+        desc:`If the distribution of letters and 'words' is random, the reader will not be distracted from making.`
+    },
+    {
+        icon:'compass',
+        title:'Handled By Expert',
+        desc:`If the distribution of letters and 'words' is random, the reader will not be distracted from making.`
+    },
+    {
+        icon:'command',
+        title:'Discussion For Idea',
+        desc:`If the distribution of letters and 'words' is random, the reader will not be distracted from making.`
+    },
+    {
+        icon:'box',
+        title:'Web Design',
+        desc:`If the distribution of letters and 'words' is random, the reader will not be distracted from making.`
+    },
+    {
+        icon:'camera',
+        title:'Network Security',
+        desc:`If the distribution of letters and 'words' is random, the reader will not be distracted from making.`
+    },
+    {
+        icon:'bell',
+        title:'Social Media',
+        desc:`If the distribution of letters and 'words' is random, the reader will not be distracted from making.`
+    },
+    {
+        icon:'monitor',
+        title:'Design & Branding',
+        desc:`If the distribution of letters and 'words' is random, the reader will not be distracted from making.`
+    },
+]
+
+export const clientData = [
+    {
+        image:client1,
+        desc:`" It seems that only fragments of the original text remain in the Lorem Ipsum texts used today. "`,
+        name:'Calvin Carlo',
+        position:'Manager',
+        rate:['mdi mdi-star','mdi mdi-star','mdi mdi-star','mdi mdi-star','mdi mdi-star']
+    },
+    {
+        image:client2,
+        desc:`" The most well-known dummy text is the 'Lorem Ipsum', which is said to have originated in the 16th century. "`,
+        name:'Christa Smith',
+        position:'Manager',
+        rate:['mdi mdi-star','mdi mdi-star','mdi mdi-star','mdi mdi-star','mdi mdi-star']
+    },
+    {
+        image:client3,
+        desc:`" One disadvantage of Lorum Ipsum is that in Latin certain letters appear more frequently than others. "`,
+        name:'Jemina CLone',
+        position:'Manager',
+        rate:['mdi mdi-star','mdi mdi-star','mdi mdi-star','mdi mdi-star','mdi mdi-star']
+    },
+    {
+        image:client4,
+        desc:`" Thus, Lorem Ipsum has only limited suitability as a visual filler for German texts. "`,
+        name:'Smith Vodka',
+        position:'Manager',
+        rate:['mdi mdi-star','mdi mdi-star','mdi mdi-star','mdi mdi-star','mdi mdi-star']
+    },
+    {
+        image:client5,
+        desc:`" There is now an abundance of readable dummy texts. These are usually used when a text is required. "`,
+        name:'Cristino Murfi',
+        position:'Manager',
+        rate:['mdi mdi-star','mdi mdi-star','mdi mdi-star','mdi mdi-star','mdi mdi-star']
+    },
+    {
+        image:client6,
+        desc:`" According to most sources, Lorum Ipsum can be traced back to a text composed by Cicero. "`,
+        name:'Cristino Murfi',
+        position:'Manager',
+        rate:['mdi mdi-star','mdi mdi-star','mdi mdi-star','mdi mdi-star','mdi mdi-star']
+    },
+]
+
+export const blogData = [
+    {
+        image:blog1,
+        date:'13th March 2025',
+        title:'Design your apps in your own way',
+        desc:'This is required when, for example, the final text is not yet available.'
+    },
+    {
+        image:blog2,
+        date:'5th May 2025',
+        title:'How apps is changing the IT world',
+        desc:'This is required when, for example, the final text is not yet available.'
+    },
+    {
+        image:blog3,
+        date:'19th June 2025',
+        title:'Smartest Applications for Business',
+        desc:'This is required when, for example, the final text is not yet available.'
+    },
+]
+
+export const socialData = [
+    {
+        link:'https://1.envato.market/upcover',
+        icon:'shopping-cart'
+    },
+    {
+        link:'https://dribbble.com/shreethemes',
+        icon:'dribbble'
+    },
+    {
+        link:'http://linkedin.com/company/shreethemes',
+        icon:'linkedin'
+    },
+    {
+        link:'https://www.facebook.com/shreethemes',
+        icon:'facebook'
+    },
+    {
+        link:'https://www.instagram.com/shreethemes',
+        icon:'instagram'
+    },
+    {
+        link:'https://twitter.com/shreethemes',
+        icon:'twitter'
+    },
+    {
+        link:'mailto:support@shreethemes.in',
+        icon:'mail'
+    },
+]
+
+export const partnerData = [
+    partner1,partner2,partner3,partner4,partner5,partner6
+]
